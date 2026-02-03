1816a30486c67bfb1cb1d0983973cb8ff84dd6eb
Yossifebrahim <yossifebrahim76@example.com>
2026-02-03 05:24:23 +0200

    avoid nosql injection

--- src/middlewares/noBody.middleware.js
+++ src/middlewares/noBody.middleware.js
@@ -1,7 +1,7 @@
 const noBodyAllowed = (req, res, next) => {
   if (req.body && Object.keys(req.body).length > 0) {
     return res.status(400).json({
-      message: "Request body is not allowed for this endpoint",
+      message: "Request body is not allowed for this endpoint!",
     });
   }
   next();
