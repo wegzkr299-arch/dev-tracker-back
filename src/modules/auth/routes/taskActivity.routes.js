2825db3d627d85a781c19a8196451b7fae3a47c0
Ali Abueldahab <wegzkr299>
2026-02-14 16:57:10 +0200

    get total hoours

--- src/modules/auth/routes/taskActivity.routes.js
+++ src/modules/auth/routes/taskActivity.routes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const TaskActivity = express.Router();
 const { protect } = require('../../../middlewares/auth.middleware');
 const { startTask, pauseTask, resumeTask, getTaskStatus, getAllSessions, getWeeklyTotalHours } = require('../controllers/takecontrollers/taskActivity');
-TaskActivity.get("/projects/developer/tasks/weekly-stats", protect, getWeeklyTotalHours);
+TaskActivity.get("/projects/developer/tasks/weekly", protect, getWeeklyTotalHours);
 TaskActivity.post("/projects/:projectId/tasks/:taskId/start", protect , startTask);
 TaskActivity.post("/projects/:projectId/tasks/:taskId/pause", protect , pauseTask);
 TaskActivity.post("/projects/:projectId/tasks/:taskId/resume" , protect , resumeTask)
