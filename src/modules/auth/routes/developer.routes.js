226d91b4b848abae427d0349519c9b1173fa0cca
Ali Abueldahab <wegzkr299>
2026-02-24 19:41:00 +0200

    change password

--- src/modules/auth/routes/developer.routes.js
+++ src/modules/auth/routes/developer.routes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const { updateUserName, otpToChangePassword, changePassword } = require('../controllers/developercotroller/developer.controller');
 const { protect } = require('../../../middlewares/auth.middleware');
-const forgotPasswordLimiter = require('../../../middlewares/rateLimit.middleware');
 const developerRouter = express.Router();
 developerRouter.patch('/changeusername' , protect ,updateUserName)
 developerRouter.post('/forgotpassword' ,otpToChangePassword)
